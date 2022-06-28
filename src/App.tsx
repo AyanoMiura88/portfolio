58d0e67ec71421bce408e7d2f29c06669c71d9b5
LILAC\marie <miura.a@marietta.co.jp>
2022-06-28 18:58:16 +0900

    [add] アバター作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import NavBar from './components/Navbar';
+import React from "react";
+import NavBar from "./components/Navbar";
+import MyAvatar from "./components/MyAvatar";
 
 const App = () => {
   return (
-    <NavBar/>
+    <>
+      <NavBar />
+      <MyAvatar />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/MyAvatar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { createStyles, styled , Theme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+
+// 画像ファイルをインポート
+import ImageFile from "../static/images/avatar.jpg";
+
+const useStyles = styled((theme: Theme) =>
+  createStyles({
+    // 表示サイズを指定
+    large: {
+      width: theme.spacing(30),
+      height: theme.spacing(30),
+    },
+  })
+);
+
+const MyAvatar: React.FC = () => {
+  const classes: any = useStyles();
+
+  return (
+    <>
+      <Box p={2}>
+        <Box display="flex" justifyContent="center" p={1} >
+          <Avatar alt="Ayano Miura" src={ImageFile} className={classes.large} />
+        </Box>
+        <Box display="flex" justifyContent="center" p={1}>
+          <Typography variant="h5">Ayano Miura</Typography>
+        </Box>
+        <Box display="flex" justifyContent="center" p={1}>
+          <Typography variant="body1">Web Enginner</Typography>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default MyAvatar;
