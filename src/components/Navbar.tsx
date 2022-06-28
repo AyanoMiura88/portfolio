b672669fe22e0a64c1c6968c35e00c0653d61c8b
LILAC\marie <miura.a@marietta.co.jp>
2022-06-28 18:19:40 +0900

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+
+const NavBar = () => {
+  return(
+    <>
+      <AppBar
+        color='default'
+        position='static'
+        style={{ alignItems: 'center'}}
+      >
+        <Toolbar>
+          <Button>
+            ABOUT
+          </Button>
+          <Button color='inherit'>
+            SKILLS
+          </Button>
+          <Button color='inherit'>
+            Works
+          </Button>
+          <Button color='inherit'>
+            CONTACT
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+};
+
+export default NavBar;
